06199d9861a29110b50faa08dec390bc040f9255
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-19 22:09:50 +0200

    Create post template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.turbo_stream
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :text)
+  end
 end
