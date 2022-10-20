2b776dda0d4512e8e74c6c852ea14c53af20939c
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-20 17:27:57 +0200

    Test validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    let(:test_user) do
+      User.create!(email: 'test@test.com', name: 'Name', password: 'Password')
+    end
+
+    subject { described_class.new(user: test_user, text: 'Text') }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without text' do
+      subject.text = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without user' do
+      subject.user_id = nil
+      expect(subject).not_to be_valid
+    end
+  end
 end
