7921984f424290b2bd558c2ceac467167ee531ce
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-21 13:53:49 +0200

    Add reply comment hiding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def render_turbo_stream_flash_messages
     turbo_stream.prepend 'flash', partial: 'layouts/flash'
   end
+
+  def show_svg(path)
+    File.open("app/assets/images/#{path}", 'rb') do |file|
+      raw file.read
+    end
+  end
 end
