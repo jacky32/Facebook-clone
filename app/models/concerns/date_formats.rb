7b6df70e069644243560d7ee35801997c5829fdc
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-24 15:26:31 +0200

    Change time to prague

--- app/models/concerns/date_formats.rb
+++ app/models/concerns/date_formats.rb
@@ -2,9 +2,9 @@ module DateFormats
   extend ActiveSupport::Concern
 
   def formatted_date
-    return created_at.strftime('%H:%M') if today?
-    return created_at.strftime('%H:%M, %-d. %B %Y') unless this_year?
-    return created_at.strftime('%H:%M, %-d. %B') unless this_year?
+    return created_at_local.strftime('%H:%M') if today?
+    return created_at_local.strftime('%H:%M, %-d. %B %Y') unless this_year?
+    return created_at_local.strftime('%H:%M, %-d. %B') unless this_year?
   end
 
   def time_difference
@@ -16,60 +16,64 @@ module DateFormats
   end
 
   def hover_date
-    created_at.strftime('%A, %B %d, %Y at %H:%M')
+    created_at_local.in_time_zone('Prague').strftime('%A, %B %d, %Y at %H:%M')
   end
 
   private
 
   def this_hour?
-    time = (Time.current - created_at.to_time) / 60
+    time = (Time.current - created_at_local.to_time) / 60
     time < 60
   end
 
   def minutes
-    "#{((Time.current - created_at.to_time) / 60).to_i}m"
+    "#{((Time.current - created_at_local.to_time) / 60).to_i}m"
   end
 
   def today?
-    date = Date.current - created_at.to_date
+    date = Date.current - created_at_local.to_date
     date < 1
   end
 
   def hours
-    hours = ((Time.current - created_at.to_time) / 3600).to_i
+    hours = ((Time.current - created_at_local.to_time) / 3600).to_i
     return "#{hours} hour ago" if hours == 1
 
     "#{hours} hours ago"
   end
 
   def this_week?
-    date = Date.current - created_at.to_date
+    date = Date.current - created_at_local.to_date
     date < 7
   end
 
   def days
-    "#{((Time.current - created_at.to_time) / 86_400).to_i} days ago"
+    "#{((Time.current - created_at_local.to_time) / 86_400).to_i} days ago"
   end
 
   def this_month?
-    date = Date.current - created_at.to_date
+    date = Date.current - created_at_local.to_date
     date < 30
   end
 
   def weeks
-    "#{((Time.current - created_at.to_time) / 604_800).to_i} weeks ago"
+    "#{((Time.current - created_at_local.to_time) / 604_800).to_i} weeks ago"
   end
 
   def this_year?
-    date = Date.current - created_at.to_date
+    date = Date.current - created_at_local.to_date
     date < 365
   end
 
   def months
-    "#{((Time.current - created_at.to_time) / 2_629_743).to_i} months ago"
+    "#{((Time.current - created_at_local.to_time) / 2_629_743).to_i} months ago"
   end
 
   def years
-    "#{((Time.current - created_at.to_time) / 31_556_926).to_i} years ago"
+    "#{((Time.current - created_at_local.to_time) / 31_556_926).to_i} years ago"
+  end
+
+  def created_at_local
+    created_at.in_time_zone('Prague')
   end
 end
