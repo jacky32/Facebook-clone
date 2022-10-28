26a98e2f18f43d069f2f6dd0eb9b0b5e9a5571a1
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-10-28 23:22:30 +0200

    Make posts/comments send with Enter

--- app/javascript/controllers/textarea_controller.js
+++ app/javascript/controllers/textarea_controller.js
@@ -5,4 +5,10 @@ export default class extends Controller {
     e.target.style.height = "auto";
     e.target.style.height = e.target.scrollHeight + "px";
   }
+
+  send(e) {
+    if (e.key == "Enter") {
+      e.target.parentNode.children[2].click();
+    }
+  }
 }
