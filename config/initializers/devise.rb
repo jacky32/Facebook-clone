6aa33686f705c730ad765b88c0bfe8432a33d7f8
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-11-03 18:17:21 +0100

    Style omniauth button

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,8 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, Rails.application.credentials.dig(:github, :GITHUB_KEY),
-                  Rails.application.credentials.dig(:github, :GITHUB_SECRET), scope: 'user'
+  config.omniauth :github, ENV.fetch('GITHUB_KEY'), ENV.fetch('GITHUB_SECRET'), scope: 'user'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
